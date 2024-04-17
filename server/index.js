c768d857919123e02d90005e3c952db9e09d3b15
Pesho <petar.panchev07@gmail.com>
2024-04-17 13:06:41 +0300

    Changing SQL version as of an authencation error

--- server/index.js
+++ server/index.js
@@ -1,7 +1,27 @@
+import mysql from "mysql2"
 import express from "express"
 
-const app = express()
+const sql_db = mysql.createConnection({
+    host:"localhost",
+    user:"root",
+    password:"root",
+    database:"test"
+})
 
+const app = express()
+// check GET request
+app.get("/", (request, response) =>{
+    response.json("Successful GET request.")
+})
+
+app.get("/phones", (request, response) =>{
+    const query = "SELECT * FROM phones"
+
+    sql_db.query(query,(data, error)=>{
+        if(error) return response.json(error)
+        return response.json(data)
+    })
+})
 app.listen(8081, ()=>{
     console.log("Listening on port 8081.")
 })
